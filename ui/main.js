48630cd40085425ad54c83205aba2e4ad992af94
Shashanthz <30816684+shashanthz@users.noreply.github.com>
2017-09-12 19:29:46 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,10 +19,30 @@
 // };
 
 var button =document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onclick=function(){
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+    //make a request to the counter end point
+    //craete a request object
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+                var counter =request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    
+  //render the variable in the correct span  
+// counter=counter+1;
+// var span=document.getElementById('count');
+// span.innerHTML=counter.toString();
 
+
+//Make the Request
+request.open('GET','http://baddamshash3434.imad.hasura-app.io/counter',true);
+request.send(null);
 };
