11c3b0afdfbf83ee84ee5edf59aad087d5c1805c
Shashanthz <30816684+shashanthz@users.noreply.github.com>
2017-09-12 19:03:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,27 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
-//change the text of the main text div
+// //change the text of the main text div
 
-var element = document.getElelmentById('main-text');
-element.innerHTML = 'New Value';
+// var element = document.getElelmentById('main-text');
+// element.innerHTML = 'New Value';
 
-//move the img
-var img=document.getElementById('madi');
+// //move the img
+// var img=document.getElementById('madi');
 
-var marginLeft=0;
-function moveRight(){
-    marginLeft = marginLeft+10;
-    img.style.marginLeft = marginLeft +'px';
-}
-img.onClick = function(){
-    var interval = setInterval(moveRight,50);
-    //img.style.marginLeft = '100px';
+// var marginLeft=0;
+// function moveRight(){
+//     marginLeft = marginLeft+10;
+//     img.style.marginLeft = marginLeft +'px';
+// }
+// img.onClick = function(){
+//     var interval = setInterval(moveRight,50);
+//     //img.style.marginLeft = '100px';
+// };
+
+var button =document.getElementById('counter');
+button.onclick=function(){
+counter=counter+1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
+
 };
