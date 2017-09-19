3c7e0adc6e70856ab3031318530ee5edf7a5e5cf
Shashanthz <30816684+shashanthz@users.noreply.github.com>
2017-09-18 19:38:53 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@ submit.onclick=function(){
     
     var names=['name1','name2','name3','name4'];
     var list='';
-    for(var i=o;i<names.length;i++){
+    for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
