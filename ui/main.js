35cfea296238ca5c66c317c8b4439b01e1ef4d2a
Shashanthz <30816684+shashanthz@users.noreply.github.com>
2017-09-18 19:53:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -63,6 +63,6 @@ submit.onclick=function(){
         list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+  //  ul.innerHTML=list;
     
 };
