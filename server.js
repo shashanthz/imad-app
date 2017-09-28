421ffa1cb9e679a25970869e8c3810c9886324db
Shashanthz <30816684+shashanthz@users.noreply.github.com>
2017-09-27 18:47:15 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'baddamshash3434',
+    database: 'baddamshash3434',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.envv.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -104,6 +113,11 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/test-db', function(req, res){
+    //make a select equest
+    //return a response
 });
 
 var counter=0;
